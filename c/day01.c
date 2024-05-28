71e6db8b93bedd59c07889477c51340c7fad4a3e
Berra Oh <berra@Berras-MacBook-Pro.local>
2024-05-28 09:36:51 +0900

    Add 20240527 Done

--- c/day01.c
+++ c/day01.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-main() {
+int main() {
     int a = 50;
     int *b = &a;
     *b = *b + 20;
-    printf("%d, %d\n", a, b);
+    printf("%d, %d\n", a, *b);
 
     char *s;
     s = "gilbut";
 
-    for (int i=0; i<6; i++) {
+    for (int i=0; i<6; i+=2) {
         printf("%c, ", s[i]);
         printf("%c, ", *(s + i));
         printf("%s\n", s+i);
     }
 }
+
+// =thread-selected,id="1"
+// 70, 70
+// g, g, gilbut
+// l, l, lbut
+// u, u, ut
