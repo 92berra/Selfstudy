6e8b7aa94e12a6e28a29c5dd01e3d16dbf866aab
92berra <ohyunwha@gmail.com>
2024-06-25 11:21:06 +0900

    Delete .ipynb

--- pytorch/fashion-mnist-classification.py
+++ pytorch/fashion-mnist-classification.py
@@ -1,10 +1,15 @@
+# Import
 import torch
 import torchvision
+
 import torchvision.transforms as transforms
+
 import torch.nn as nn
 import torch.nn.functional as F
+
 import torch.optim as optim
 
+# Dataset
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 trainset = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
@@ -26,3 +31,41 @@ class Net(nn.Module):
         return x
 
 net = Net()
+
+# Loss function
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+
+# Train
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print(f'[Epoch {epoch + 1}, Mini-batch {i + 1}] Loss: {running_loss / 100:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+# Evaluation
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct / total:.2f}%')
