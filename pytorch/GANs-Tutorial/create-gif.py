77727c1faf677297feb98362571b2f0e5a83678e
92berra <ohyunwha@gmail.com>
2024-07-14 00:57:06 +0900

    Delete: sample

--- pytorch/GANs-Tutorial/create-gif.py
+++ pytorch/GANs-Tutorial/create-gif.py
@@ -3,11 +3,19 @@ import imageio
 
 def create_gif(png_dir, gif_path, duration=0.5):
     images = []
-    for file_name in sorted(os.listdir(png_dir)):
-        if file_name.endswith('.png'):
+
+    for i in range(len(png_dir)):
+        
+        if i % 100 == 0:
             file_path = os.path.join(png_dir, file_name)
             images.append(imageio.imread(file_path))
     imageio.mimsave(gif_path, images, duration=duration)
+
+    # for file_name in sorted(os.listdir(png_dir)):
+    #     if file_name.endswith('.png'):
+    #         file_path = os.path.join(png_dir, file_name)
+    #         images.append(imageio.imread(file_path))
+    # imageio.mimsave(gif_path, images, duration=duration)
 
 png_dir = 'Lab1-VanillaGAN/result/2/sample'
 gif_path = 'Lab1-VanillaGAN/result/2/sample_animation.gif'
